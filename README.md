34960b3533b9b30ffb370e6db96f89641d4a730e
Aleksandr <alxfro@yandex.ru>
2018-03-06 11:44:54 +0300

    Нужный софт. Интерфейс. Ключи. Задел под ядро.

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # docker-lab
 Эко-система управления проектом в Docker
 
+Запуск:
+./control.sh
+
+Ключи:
+./control.sh -x проверка поддержки ключей
+
 Создано тут - http://www.fdev.biz
--- control.sh
+++ control.sh
@@ -1,10 +1,89 @@
 #!/bin/bash
-needlePackage=vrotmnenogi
-needleName=ВротМнеНоги
-command -v $needlePackage >/dev/null 2>&1 || { echo >&2 "[ WARNING !] 
-Для дальнейшей работы потребуется программа $needleName.
-Пробую установить $needleName...
-Сейчас потребуется пароль пользователя root.
-"; sudo apt-get install $needlePackage; command -v $needlePackage >/dev/null 2>&1 || { echo "
-Не удалось установить $needleName. Аварийной завершение работы. 
-Пожалуйста, установите $needleName вручную чтобы продолжить работу."; exit 1; } }
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - - - - - - 
+version="0.1 Alpha"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - - - - - - 
+# Требуемый софт для работы Docker Lab.
+initialSoftware=(docker docker-compose dialog git)
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - - - - - - 
+# Инсталлируем требуемые пакеты и проверяем поставилось ли.
+function install {	
+	command -v $needlePackage >/dev/null 2>&1 || { echo >&2 "[ WARNING !] 
+	Для дальнейшей работы потребуется программа $needlePackage.
+	Пробую установить $needlePackage...
+	Сейчас потребуется пароль пользователя root.
+	"; sudo apt-get install $needlePackage; command -v $needlePackage >/dev/null 2>&1 || { echo "
+	Не удалось установить $needlePackage. Аварийной завершение работы. 
+	Пожалуйста, установите $needlePackage вручную чтобы продолжить работу."; exit 1; } }
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - - - - - - 
+# Говори юзеру что-то либо нативно в консоли либо в диалоге
+function blablabla {
+	# todo - эту хрень заменить на command -v
+	I=`dpkg -s dialog | grep "Status" ` #проверяем состояние пакета (dpkg) и ищем в выводе его статус (grep)
+if [ -n "$I" ] #проверяем что нашли строку со статусом (что строка не пуста)
+then
+   dialog --title "Docker Lab:" --msgbox "$*" 12 40
+else
+   echo $1
+fi	
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - - - - - - 
+# Инициализация Docker Lab.
+function init {
+	echo "Запуск Docker Lab..."
+
+	# Смотрим, есть ли весь нужный нам софт.
+	for needlePackage in ${initialSoftware[*]}
+	do
+	    install $needlePackage
+	done
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - - - - - - 
+# Если инициализация Docker Lab прошла успешно говорим юзеру Привет.
+function helloWorld {
+	dialog --title 'Запуск Docker Lab' --yesno 'Запускаем Docker Lab?' 10 40
+ 
+	case "$?" in
+	    '0')		
+		blablabla "Данная версия $version находится в разработке и будет завершена. Для тестирования работы параметров можно запустить с ключом -x"	    	   
+	    ;;
+	    '1')
+	    echo "До новых встреч!"
+	    exit
+	    ;;
+	    '-1')
+	    echo 'Вы вышли, нажав ESC. До новых встреч!'
+	    exit 1
+	    ;;
+	esac
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - - - - - - 
+# void() method :) который собирает всё-всё-всё.
+function void {
+	# Инициализация
+	init initialSoftware
+	# Запускаемся
+	helloWorld	
+	# Уходим в закат
+	blablabla "Docker Lab завершил работу."
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - - - - - - 
+# Основное тело - проверки, запуск VOID.
+# Задел на будущее - поддержка переменных для работы в SILENT режиме.
+while [ -n "$1" ]
+do
+case "$1" in
+-x) 
+blablabla "Однажду, в студёною зимнюю пору, лошадка примерзла пиписькой к забору."
+exit
+;;
+#-b) echo "Нашли -y option" ;;
+#-c) echo "Нашли -z option" ;;
+#*) echo "$1 такой ключ нельзя использовать." exit;; #исправить эту хрень.
+esac
+shift
+done
+
+void
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - - - - - - 
+# Ты достиг дна.
